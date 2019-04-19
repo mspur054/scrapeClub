a068c7792f9fe65d8a11e4f23f7a98f1050c874e
mspur054 <mspur054@uottawa.ca>
2019-04-18 21:39:01 -0400

    Get Athlete follow stats

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { getHTML, getClubStats } from "./lib/scraper";
+import { getHTML, getClubStats, getAthleteStats } from "./lib/scraper";
+import { get } from "https";
 
 async function ScrapeData() {
-  const html = await getHTML("https://www.strava.com/clubs/473964");
-  const clubMembers = await getClubStats(html);
+  //const html = await getHTML("https://www.strava.com/clubs/473964");
+  //const clubMembers = await getClubStats(html);
+  const html = await getHTML("https://www.strava.com/athletes/11205099");
 
-  console.log(`The club has ${clubMembers} members`);
+  const ath = await getAthleteStats(html);
+
+  //console.log(`The club has ${clubMembers} members`);
 }
 
 ScrapeData();
