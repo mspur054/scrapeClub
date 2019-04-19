d7d524f4855720b28df64b73fe5f3e03aa924254
mspur054 <mspur054@uottawa.ca>
2019-04-18 22:56:44 -0400

    Got pro scraping working

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-import { getHTML, getClubStats, getAthleteStats } from "./lib/scraper";
+import {
+  getHTML,
+  getClubStats,
+  getAthleteStats,
+  getProStats
+} from "./lib/scraper";
 import { get } from "https";
 
 async function ScrapeData() {
   //const html = await getHTML("https://www.strava.com/clubs/473964");
   //const clubMembers = await getClubStats(html);
-  const html = await getHTML("https://www.strava.com/athletes/11205099");
+  const html = await getHTML("https://www.strava.com/pros/1635688");
+  //Tim 11205099
+  //Sage
 
-  const ath = await getAthleteStats(html);
-
+  // const ath = await getAthleteStats(html);
+  const prop = await getProStats(html);
   //console.log(`The club has ${clubMembers} members`);
 }
 
