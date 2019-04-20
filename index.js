1bf70ebc704387accdebb55b4419df750ade94be
mspur054 <mspur054@uottawa.ca>
2019-04-19 21:34:24 -0400

    added cron job

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
+import db from "./lib/db";
 import {
   getHTML,
   getClubCount,
   getAthleteStats,
   getProStats
 } from "./lib/scraper";
+import "./lib/cron";
 
 const app = express();
 
-app.get("/scrape", async (req, res, next) => {
-  console.log("scraping!");
+console.log(db);
 
+app.get("/scrape", async (req, res, next) => {
+  console.log("scraping!!");
   const jimPromise = getHTML("/pros/1635688");
   const sagePromise = getHTML("/pros/1595767");
   const timPromise = getHTML("/athletes/11205099");
@@ -36,15 +39,7 @@ app.get("/scrape", async (req, res, next) => {
     getAthleteStats(mattHTML)
   ]);
 
-  console.log(
-    jimFollowers,
-    sageFollowers,
-    timFollowers,
-    mattFollowers,
-    cClubCount
-  );
-
-  res.json({ jimFollowers, sageFollowers, timFollowers, mattFollowers });
+  res.json({ jimFollowers, sageFollowers, mattFollowers, timFollowers });
 });
 
 app.listen(2066, () => console.log("running on port 2066"));
